251f90bb23802e66a2f7653a25189e8ffc8ffce2
306 <306@306NEW-15>
2023-06-05 13:33:09 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = double.Parse(Console.ReadLine());
+            double f = 5 * Math.Cos(2*x);
+            Console.WriteLine("Результат"+f);
+            Console.ReadKey();
+        }
+    }
+}
